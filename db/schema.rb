36e44042f25e743df1b5f8395d9d143562983e68
samroodAli <devsamrood@gmail.com>
2021-04-21 16:28:05 +0530

    Rename posts_id to post_id in comments table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_21_102001) do
+ActiveRecord::Schema.define(version: 2021_04_21_105725) do
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
@@ -26,7 +26,7 @@ ActiveRecord::Schema.define(version: 2021_04_21_102001) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
-    t.integer "posts_id"
+    t.integer "post_id"
   end
 
   create_table "posts", force: :cascade do |t|
