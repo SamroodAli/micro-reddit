e004f9d794cc7e2a82637773ef62612ddfb81709
Juliana Osemeke <juliana.osemeke@gmail.com>
2021-04-21 12:10:48 +0100

    Correct linter error

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  validates :name , presence: true, length: {maximum: 50}
-  validates :email , presence: true
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :email, presence: true
   has_many :posts
   has_many :comments
 end
