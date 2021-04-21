d0e2033a34894b65c1b1a4f1720244be864fcf3a
samroodAli <devsamrood@gmail.com>
2021-04-21 16:33:36 +0530

    Remove blogs table from database scema

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2021_04_21_105725) do
-
-  create_table "blogs", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
-    t.integer "user_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
+  
   create_table "comments", force: :cascade do |t|
     t.string "name"
     t.text "body"
