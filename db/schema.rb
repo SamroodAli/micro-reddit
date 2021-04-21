7e95730905fcc7050f33472bcac177051ab2e676
Juliana Osemeke <juliana.osemeke@gmail.com>
2021-04-21 09:29:43 +0100

    create table for blogs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_21_081729) do
+ActiveRecord::Schema.define(version: 2021_04_21_082350) do
+
+  create_table "blogs", force: :cascade do |t|
+    t.string "title"
+    t.string "string"
+    t.string "body"
+    t.string "text"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
